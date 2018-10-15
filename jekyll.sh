dae042cd25f76db131ee1d4b65acff2e0dab63f9
Phill Conrad <pconrad@cs.ucsb.edu>
2018-10-15 08:51:58 -0700

    change site name

--- jekyll.sh
+++ jekyll.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
+
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+
+rvm use 2.5.1
+
 bundle exec jekyll serve
--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+
 echo "Installing software needed to run Jekyll locally... "
-rvm install ruby-2.4.0
-rvm use 2.4.0
+rvm install ruby-2.5.1
+rvm use 2.5.1
 gem install bundler
 bundle install --path vendor/bundle
 echo "Done."
