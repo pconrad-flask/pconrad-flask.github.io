cf22d1105f05edfb2b99a68550986f2391ca28dd
Phill Conrad <pconrad@cs.ucsb.edu>
2018-10-29 16:39:49 -0700

    update gems

--- setup.sh
+++ setup.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
-
 echo "Installing software needed to run Jekyll locally... "
+
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
+[[ -s "/etc/profile.d/rvm.sh" ]] && source "/etc/profile.d/rvm.sh"
+
+
 rvm install ruby-2.5.1
 rvm use 2.5.1
 gem install bundler
 bundle install --path vendor/bundle
 echo "Done."
+
