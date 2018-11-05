29aa52594d66c6ec479393a809b114e61c8cd875
Phill Conrad <pconrad@cs.ucsb.edu>
2018-11-05 09:59:51 -0800

    Create matplotlib.py

---
+++ _topics/matplotlib.py
@@ -0,0 +1,6 @@
+---
+topic: "Matplotlib"
+desc: "Producing plots inside a Flask Webapp without touching file system (so it works on Heroku)"
+---
+
+See: <https://github.com/pconrad-webapps/matplotlib-flask-demo>
